473d34db2ea63ba1a2613a0afd1f3f1746b55f03
tanvir321110 <tanvirahmed321110@gmail.com>
2025-05-18 15:51:23 +0600

    task view page added

--- static/js/index.js
+++ static/js/index.js
@@ -1,14 +1,45 @@
 
 
 
-// for tab button
-document.querySelectorAll('.regular-assign-btn button').forEach(button => {
-    button.addEventListener('click', function () {
-        // Remove active class from siblings
-        this.parentElement.querySelectorAll('button').forEach(btn => {
-            btn.classList.remove('active');
-        });
-        // Add active to clicked button
-        this.classList.add('active');
-    });
+
+
+
+
+
+
+// Total Purchases Over Time (Line Chart)
+const totalPurchasesCtx = document.getElementById('totalPurchasesChart').getContext('2d');
+new Chart(totalPurchasesCtx, {
+    type: 'bar',
+    data: {
+        labels: ['1-5', '6-10', '11-15', '16-20', '21-25', '26-30'],
+        datasets: [{
+            label: 'Total Purchases',
+            data: [1200, 1900, 3000, 2500, 3200, 1200],
+            borderColor: 'rgba(75, 192, 192)',
+            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+            tension: 0.4
+        }]
+    },
+    options: {
+        responsive: true
+    }
+});
+
+// Product Types (Pie Chart)
+const productTypesCtx = document.getElementById('productTypesChart').getContext('2d');
+new Chart(productTypesCtx, {
+    type: 'doughnut',
+    data: {
+        labels: ['পাউরুটি', 'বিস্কুট', 'পেস্ট্রি', 'কুকি'],
+        datasets: [{
+            label: 'Product Types',
+            data: [40, 25, 20, 15],
+            backgroundColor: ['#ff6384', '#36a2eb', '#ffcd56', '#4bc0c0'],
+            hoverOffset: 4
+        }]
+    },
+    options: {
+        responsive: true
+    }
 });
---
+++ static/js/my_task.js
@@ -0,0 +1,11 @@
+// for tab button
+document.querySelectorAll('.regular-assign-btn button').forEach(button => {
+    button.addEventListener('click', function () {
+        // Remove active class from siblings
+        this.parentElement.querySelectorAll('button').forEach(btn => {
+            btn.classList.remove('active');
+        });
+        // Add active to clicked button
+        this.classList.add('active');
+    });
+});
